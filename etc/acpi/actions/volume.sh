8f88d040a6402fef655134a4aecf79921cf95a72
arbitrary-dev <arbitrary-dev@users.noreply.github.com>
2020-02-01 22:15:48 +0200

    add volume support for BT devices

--- etc/acpi/actions/volume.sh
+++ etc/acpi/actions/volume.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+HANDLE="`amixer -D bluealsa | grep A2DP | cut -d\' -f2`"
+DEVICE="${HANDLE:+bluealsa}"
+
+HANDLE="${HANDLE:-Master}"
+DEVICE="${DEVICE:-default}"
+
 case "$1" in
-  *up)   amixer -q -M set Master playback 5%+ ;;
-  *down) amixer -q -M set Master playback 5%- ;;
-  *mute) amixer -q set Master playback toggle ;;
+  *up)   amixer -D "$DEVICE" -q -M set "$HANDLE" playback 5%+ ;;
+  *down) amixer -D "$DEVICE" -q -M set "$HANDLE" playback 5%- ;;
+  *mute) amixer -D "$DEVICE" -q set "$HANDLE" playback toggle ;;
 esac
