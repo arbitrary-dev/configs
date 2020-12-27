fc7a4cd2cc38d3b2f780e9c53cf79ea211314e6e
arbitrary-dev <arbitrary-dev@users.noreply.github.com>
2020-12-27 21:35:35 +0200

    Migrate to pulseaudio

--- etc/acpi/actions/volume.sh
+++ etc/acpi/actions/volume.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-HANDLE="`amixer -D bluealsa | grep A2DP | cut -d\' -f2`"
-DEVICE="${HANDLE:+bluealsa}"
+#HANDLE="`amixer -D bluealsa | grep A2DP | cut -d\' -f2`"
+#DEVICE="${HANDLE:+bluealsa}"
 
 HANDLE="${HANDLE:-Master}"
-DEVICE="${DEVICE:-default}"
+DEVICE="${DEVICE:-pulse}"
+USER=man
 
 case "$1" in
-  *up)   amixer -D "$DEVICE" -q -M set "$HANDLE" playback 5%+ ;;
-  *down) amixer -D "$DEVICE" -q -M set "$HANDLE" playback 5%- ;;
-  *mute) amixer -D "$DEVICE" -q set "$HANDLE" playback toggle ;;
+  *up)   su $USER -c "amixer -D '$DEVICE' -q -M set '$HANDLE' playback 5%+" ;;
+  *down) su $USER -c "amixer -D '$DEVICE' -q -M set '$HANDLE' playback 5%-" ;;
+  *mute) su $USER -c "amixer -D '$DEVICE' -q set '$HANDLE' playback toggle" ;;
 esac
