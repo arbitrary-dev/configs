97f9b5302a221cbd9e88a6b26bcb5168e32b9272
arbitrary-dev <arbitrary-dev@users.noreply.github.com>
2022-06-08 21:30:45 +0300

    upd

--- home/dolf/.config/sway/status.sh
+++ home/dolf/.config/sway/status.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 while true; do
+  MEMORY=`
+    free --human \
+    | awk '{print $3}' \
+    | sed -nE "s/(.+[GM])i/\1/p" \
+    | awk '{if (NR==1) printf "%s ",$0; else printf "(%s) ",$0}'`" "
+
   BAT=`upower -i /org/freedesktop/UPower/devices/battery_BAT0`
   CHARGE=`sed -En '/percentage/{s/.*: +(.+)/\1/p}' <<< $BAT`
   STATE=`sed -En '/state/{s/.*: +(.+)/\1/p}' <<< $BAT`
@@ -9,10 +15,10 @@ while true; do
   TIMETO=`sed -En '/time to/{s/.*: +(.+)/\1/p}' <<< $BAT`
   [ ! -z "$TIMETO" ] && TIMETO=" ($STATE$TIMETO)"
   BATTERY=
-  [ "$STATE" != "fully-charged" ] && BATTERY="$CHARGE$TIMETO"
+  [ "$STATE" != "fully-charged" ] && BATTERY="$CHARGE$TIMETO  "
 
   DATETIME=`date +'%Y-%m-%d %H:%M'`
 
-  echo "$BATTERY $DATETIME "
+  echo "$BATTERY$MEMORY$DATETIME "
   sleep 61
 done
