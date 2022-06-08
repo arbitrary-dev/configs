ca4a49a29ee5d2fafca9d36ce827dc93ec1c6e8b
arbitrary-dev <arbitrary-dev@users.noreply.github.com>
2022-06-08 21:28:17 +0300

    Fix double suspend in Sway

--- lib64/elogind/system-sleep/hooks.sh
+++ lib64/elogind/system-sleep/hooks.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 if [ "$1" == "pre" ]; then
-  sudo -u dolf sh -c 'XDG_RUNTIME_DIR=/run/user/1000 WAYLAND_DISPLAY=wayland-1 swaylock'
+  pgrep swaylock >/dev/null \
+  || sudo -u dolf sh -c 'XDG_RUNTIME_DIR=/run/user/1000 WAYLAND_DISPLAY=wayland-1 swaylock'
+  # Add some delay to avoid double suspend in Sway.
+  sleep 2
 fi
